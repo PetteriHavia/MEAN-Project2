561c5599ac2c94a18bb836cece8295cb0630be6b
PetteriH <petsku15@hotmail.fi>
2020-03-28 17:41:17 +0200

    Small fix

--- index.js
+++ index.js
@@ -24,8 +24,6 @@ app.get('/', function (req, res) {
 
 
 
-
-
 app.get('/guestbook', function (req, res) {
   //Place json data into a variable and pass it to guestbook.ejs-template
   var jsonData = require('./views/pages/json_guestbook_data.json');
@@ -44,8 +42,28 @@ app.get('/newmessage', function (req, res) {
 
 //NEWMESSAGE.POST
 app.post('/newmessage', function (req, res) {
+
+  var data = require("./views/pages/json_guestbook_data.json");
+
+    //Push data
+    data.push({
+        "username": req.body.name,
+        "country": req.body.country,
+        "message": req.body.message,
+        "date": new Date()
+    });
+    
+    //Convet the JSON object
+    var jsonStr = JSON.stringify(data);
+
+    //Write the new information in the file
+    fs.writeFile('./views/pages/json_guestbook_data.json', jsonStr, (err) => {
+        if(err) throw err;
+        console.log("Data saved");
+    });
+    res.send("Saved data to file json_guestbook_data.json type /guestbook to have a look")
   
-  fs.readFile('./views/pages/json_guestbook_data.json', 'utf-8', function(err, data) {
+  /*fs.readFile('./views/pages/json_guestbook_data.json', 'utf-8', function(err, data) {
     if (err) throw err
   
     var jsonArray = JSON.parse(data)
@@ -56,7 +74,7 @@ app.post('/newmessage', function (req, res) {
       "country": req.body.country,
       "message": req.body.message,
       "date": new Date()
-    })
+    });
     
     console.log(jsonArray);
 
@@ -65,15 +83,57 @@ app.post('/newmessage', function (req, res) {
       if(err) throw err;
       console.log("Data saved");
     });
-    res.send("Saved data to file json_guestbook_data.json type /guestbook to have a look");
-  })
+    res.send("Saved data to file json_guestbook_data.json type /guestbook to have a look");Ä
+  })*/
 });
 
 
+app.post('/ajaxmessage', function (req, res) {
+  
+  
+  var name = req.body.name;
+  var country = req.body.country;
+  var message = req.body.message
+  console.log(name, country, message);
+
+  var data = require("./views/pages/json_guestbook_data.json");
+
+  //Push data
+  data.push({
+      "username": req.body.name,
+      "country": req.body.country,
+      "message": req.body.message,
+      "date": new Date()
+  });
+  
+  //Convet the JSON object
+  var jsonStr = JSON.stringify(data);
+
+  //Write the new information in the file
+  fs.writeFile('./views/pages/json_guestbook_data.json', jsonStr, (err) => {
+      if(err) throw err;
+      console.log("Data saved"); 
+  });
+
+  //Parse data
+  var result = '<table border="1" align="center">';
+
+  for (var i=0; i < data.length; i++) {
+      result +=
+      '<tr>' +
+          '<td>'+ data[i].username +'</td>' +
+          '<td>'+ data[i].country +'</td>' +
+          '<td>'+ data[i].message +'</td>' +
+          '<td>'+ data[i].date +'</td>' +
+      '</tr>'
+  }
+  res.send(result);
+});
+
 app.get('/ajaxmessage', function (req, res) {
-  res.render('pages/ajaxmessage', {
+    res.render('pages/ajaxmessage', {
     new_header: "/AJAXMESSAGE PAGE",
-    new_headerTwo: "SAVED DATA BELOW",
+    new_headerTwo: "SAVED DATA BELOW"
   });
 });
 
